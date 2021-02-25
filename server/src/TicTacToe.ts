e763e2913f35e98d73812712f8b797c7598a8330
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-02-25 12:24:02 +0300

    Added function to check winner

--- server/src/TicTacToe.ts
+++ server/src/TicTacToe.ts
@@ -12,24 +12,17 @@ class TicTacToe {
   id: string = randomString({ length: 5 });
   players: Player[] = [];
   limit = 2;
+  score: { [name: string]: number } = {};
+  numberOfMoves = 0;
   turn: number;
-  board: string[][] = [
-    [undefined,undefined,undefined],
-    [undefined,undefined,undefined],
-    [undefined,undefined,undefined]
-  ];
+  board: string[][];
   join(user: Player): void {
     if(this.players.length < this.limit){
       this.players.push(user);
+      this.score[user.socket.id] = 0;
       user.socket.emit("JOINED_GAME", this.id);
       if(this.players.length === this.limit){
-        this.turn = Math.random() > 0.5 ? 1 : 0;
-        const allPlayers = this.players.map(player => ({ id: player.socket.id, nick: player.nick }));
-        this.players.map(player => player.socket.emit("GAME_STARTED", {
-          turn: this.players[this.turn].socket.id,
-          board: this.board,
-          players: allPlayers
-        }));
+        this.rematch();
       }else {
         this.players.map(player => player.socket.emit("WAITING_FOR_OPPONENT"));
       }
@@ -49,7 +42,88 @@ class TicTacToe {
         turn: this.players[this.turn].socket.id,
         board: this.board,
       }));
+      this.numberOfMoves++;
+      this.check();
     }
+  }
+  check(): void {
+    // cols
+    for(let x = 0; x < 3; x++) {
+      let sign: string;
+      let win = true;
+      for(let y = 0; y < 3; y++) {
+        const value = this.board[x][y];
+        if (!value) {
+          win = false;
+          break;
+        }
+        if (!sign) sign = value;
+        if (sign !== value) {
+          win = false;
+          break;
+        }
+      }
+      if(win) {
+        this.score[sign]++;
+        this.players.map(player => player.socket.emit("GAME_END", { winner: sign, score: this.score }));
+        this.rematch();
+        return;
+      }
+    }
+    // row
+    for(let y = 0; y < 3; y++) {
+      let sign: string;
+      let win = true;
+      for(let x = 0; x < 3; x++) {
+        const value = this.board[x][y];
+        if (!value) {
+          win = false;
+          break;
+        }
+        if (!sign) sign = value;
+        if (sign !== value) {
+          win = false;
+          break;
+        }
+      }
+      if(win) {
+        this.score[sign]++;
+        this.players.map(player => player.socket.emit("GAME_END", { winner: sign, score: this.score }));
+        this.rematch();
+        return;
+      }
+    }
+    // diagonal
+    if((this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) || (this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0])){
+      if(this.board[1][1]){
+        this.score[this.board[1][1]]++;
+        this.players.map(player => player.socket.emit("GAME_END", { winner: this.board[1][1], score: this.score }));
+        this.rematch();
+        return;
+      }
+    }
+    // draw
+    if(this.numberOfMoves === 9){
+      this.players.map(player => player.socket.emit("GAME_END", { draw: true, score: this.score }));
+      this.rematch();
+      return;
+    }
+  }
+  rematch(): void {
+    this.turn = Math.random() > 0.5 ? 1 : 0;
+    this.numberOfMoves = 0;
+    this.board = [
+      [undefined,undefined,undefined],
+      [undefined,undefined,undefined],
+      [undefined,undefined,undefined]
+    ];
+    const allPlayers = this.players.map(player => ({ id: player.socket.id, nick: player.nick }));
+    this.players.map(player => player.socket.emit("GAME_STARTED", {
+      turn: this.players[this.turn].socket.id,
+      board: this.board,
+      score: this.score,
+      players: allPlayers
+    }));
   }
 }
 
